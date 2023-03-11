f8d1c0043483cee34eafe3a3dbc9ef3a6f9e5f83
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-03-10 19:24:49 -0800

    Remove all api routes

--- api/test/orders.js
+++ api/test/orders.js
@@ -3,22 +3,22 @@ const { Cart, CartProduct } = require("../api/db/index");
 
 const { requireToken } = require("../api/middleware");
 
-router.get("/", requireToken, async (req, res, next) => {
-  try {
-    const userOrder = await Cart.findOne({
-      where: {
-        userId: req.user.id,
-        isCart: false,
-      },
-      order: [["updatedAt", "DESC"]],
-      include: {
-        model: CartProduct,
-      },
-    });
-    res.json(userOrder);
-  } catch (err) {
-    next(err);
-  }
-});
+// router.get("/", requireToken, async (req, res, next) => {
+//   try {
+//     const userOrder = await Cart.findOne({
+//       where: {
+//         userId: req.user.id,
+//         isCart: false,
+//       },
+//       order: [["updatedAt", "DESC"]],
+//       include: {
+//         model: CartProduct,
+//       },
+//     });
+//     res.json(userOrder);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 module.exports = router;
