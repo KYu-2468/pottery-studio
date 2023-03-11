fb9dd45573a256e0036d33646b0379fc9f798ac5
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-03-10 18:46:51 -0800

    Comment all api routes

--- api/api/index.js
+++ api/api/index.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.use('/products', require('./products'));
-router.use('/users', require('./users'));
-router.use('/auth', require('./auth'));
-router.use('/admin', require('./admin'));
-router.use('/cart', require('./cart'));
-router.use('/guest', require('./guest'));
-router.use('/orders', require('./orders'));
+router.use("/products", require("./products"));
+// router.use('/users', require('./users'));
+// router.use('/auth', require('./auth'));
+// router.use('/admin', require('./admin'));
+// router.use('/cart', require('./cart'));
+// router.use('/guest', require('./guest'));
+// router.use('/orders', require('./orders'));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found');
+  const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
