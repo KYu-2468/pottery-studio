5785c651eff946a5b3a43b94565964c57992d9ee
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-03-10 19:21:09 -0800

    Remove all api routes

--- api/test/index.js
+++ api/test/index.js
@@ -5,7 +5,7 @@ router.use("/users", require("./users"));
 router.use("/auth", require("./auth"));
 // router.use("/admin", require("./admin"));
 router.use("/cart", require("./cart"));
-router.use("/guest", require("./guest"));
+// router.use("/guest", require("./guest"));
 router.use("/orders", require("./orders"));
 
 // router.use((req, res, next) => {
