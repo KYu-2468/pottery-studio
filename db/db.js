9b15eb0d52c236dcfa05b6a9e75000188d6cf532
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-03-12 13:54:33 -0700

    Vercel

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const pkg = require("../package.json");
+const pg = require("pg");
 
 const databaseName =
   pkg.name + (process.env.NODE_ENV === "test" ? "-test" : "");
